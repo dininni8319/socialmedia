671a9f2149f4c88720e39fde134be6b916bdf0ff
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2023-08-30 17:26:06 +0200

    Navbar and style implementations

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,9 +1,49 @@
+import {
+  Box,
+  Typography,
+  useTheme,
+  useMediaQuery,
+ } from "@mui/material";
+import Form from "./Form.jsx";
 
 const LoginPage = () => {
+  const theme = useTheme()
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)")
+
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <Box>
+      <Box width='100%'
+        p='1rem 6%'
+        display='flex'
+        textAlign='center'
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography
+          fontWeight="bold"
+          fontSize="32px"
+          color='primary'
+        >Login Page
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p='2rem'
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography
+          fontWeight="500"
+          variant="h5"
+          sx={{ mb: "1.5rem"}}
+        >
+          Welcome to Sociopedia, the Social Media for Sociopaths!
+        </Typography>
+        <Form>
+
+        </Form>
+      </Box>
+    </Box>
   );
 }
 
