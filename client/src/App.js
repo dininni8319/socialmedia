51d3ef352d9b9fbf487bac787b01e36d878d0946
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2023-08-31 16:15:28 +0200

    completed login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -12,15 +12,22 @@ import { themeSettings } from './theme'
 const App = () => {
   const mode = useSelector(state => state.mode)
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+  const isAuth = Boolean(useSelector((state) => state.token))
   return (
     <div className="App">
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route path="/home" element={<HomePage />} />
+            <Route 
+              path="/home" 
+              element={isAuth ?<HomePage /> : <Navigate to='/' />} 
+            />
             <Route path="/" element={<LoginPage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route 
+              path="/profile/:userId" 
+              element={isAuth  ?<ProfilePage /> : <Navigate to='/' />} 
+            />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </ThemeProvider>
